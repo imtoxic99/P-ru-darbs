cef018ba15b24fb66027988fdb534dc753ac19d5
imtoxic99 <93687654+imtoxic99@users.noreply.github.com>
2025-12-18 08:59:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ viz.go()
 model = viz.add('models/tinker.obj')
 model.setPosition([0, 0, 5])
 model.setScale([0.1, 0.1, 0.1])
+
+def onPick(e):
+    if e.object == model:
+        model.color(viz.RED)
+        model.addAction(vizact.moveTo([0, 1.5, 5], time=1))
+
+viz.callback(viz.PICK_EVENT, onPick)
