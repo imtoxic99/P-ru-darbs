595497e22e6b7ac5dbcecb8c158367b93cef9b5d
imtoxic99 <93687654+imtoxic99@users.noreply.github.com>
2025-12-18 09:03:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import viz
 import vizact
+import vizcam
 
 viz.go()
 viz.mouse(viz.ON)
 
+# Камера: ходьба + мышь
+vizcam.WalkNavigate()
+viz.MainView.setPosition([0, 1.6, 0])
+
+# Модель
 model = viz.add('models/tinker.obj')
 model.setPosition([0, 0, 5])
 model.setScale([0.1, 0.1, 0.1])
 
+# Интерактивность: клик
 def onPick():
     model.color(viz.RED)
     model.addAction(vizact.moveTo([0, 1.5, 5], time=1))
 
 vizact.onpick(model, onPick)
-
-viz.walkNavigate()
-viz.MainView.setPosition([0, 1.6, 0])
